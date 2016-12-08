06025466b1e4c365f8080c4d017ecfe71cbf8698
Danny <danny.gosain@hotmail.com>
2016-12-08 14:45:49 -0500

    create new list

---
+++ main.js
@@ -0,0 +1,143 @@
+$(document).ready(function() {
+	var authenticationSuccess = function() {
+	    var kanban = '58487edd7b75ece246c80b59';
+
+	    var UCD_Board = '5849b7b17f474c77d0eb314a';
+
+		var failure = function() {
+			console.log("Tu chutiya hai");
+		}
+
+		var creationSuccess = function(data) {
+		  console.log('Card created successfully.');
+		};
+		
+		var addSuccess = function(data,board){
+			console.log("Blah");
+		}
+
+		var addSuccessUCD = function(data) {
+			console.log("List added");
+			// addSuccess(data,UCD_Board);
+		}
+
+		var addSuccessJenkins = function(data){
+			addSuccess(data,JenkinsBoard_Triage);
+		}
+
+		var getSuccess = function(data) {
+			var labelNames = data.labelNames; //json/dictionary
+			var allCards = data.cards; //array
+			var allActions = data.actions; //array
+			var allLists = data.lists;
+
+			for (var i = 0; i < allActions.length; i++){
+				var actionItem = allActions[i].type;
+
+				if(actionItem == "createList"){
+					var newList = {
+						name: actionItem[i].data.board.name,
+						idBoard: UCDBoard,
+						pos: 'top'
+					}
+					Trello.post('/lists/',newList,addSuccessUCD);
+				}
+			}
+
+			//going through all cards and put it on the right board
+			// for (var i = 0; i < allCards.length; i++){
+			// 	var cardLabels = allCards[i].labels;
+			// 	//going through the card label
+			// 	for (var j = 0; j < cardLabels; j++){
+			// 		if (cardLabels[j].name == "UCD"){
+			// 			//create card in UCD with card id
+			// 			Trello.cards.get(allCards[i].id,addSuccessUCD,failure);
+			// 		} else if (cardLabels[j].name == "Jenkins"){
+			// 			//create card in Jenkins with card id
+			// 			// Trello.cards.get(allCards[i].id,addSuccessJenkins,failure);
+			// 			console.log("Jenkins card detected");
+			// 		} else {
+			// 			console.log("Unknown board");
+			// 		}
+			// 	}
+			// }
+			
+		};
+
+		Trello.boards.get(kanban,getSuccess,failure);
+
+	};
+
+	var authenticationFailure = function() {
+	    console.log("Failed authentication");
+	};
+
+	$('#update').click(function() {
+		Trello.authorize({
+			type: 'popup',
+			name: 'Getting Started Application',
+			scope: {
+				read: 'true',
+				write: 'true' },
+			expiration: 'never',
+			success: authenticationSuccess,
+			error: authenticationFailure
+		});
+	});
+});
+
+
+// OLD CODE
+
+// $(document).ready(function() {
+// 	var authenticationSuccess = function() {
+// 	    console.log("Successful authentication");
+// 	    var kanban = '58487edd7b75ece246c80b59';
+
+// 	    var UCDBoard = '58487ee330aab261299ef1b1';
+// 	    var UCDBoard_Triage = '5848860065df2e7765bf9900';
+// 	    var UCDBoard_Backlog = '584985b734ef65fb7e793e2f';
+// 	    var UCDBoard_Refined = '584985c3d663dfa63498651d';
+// 	    var UCDBoard_DoIt = '584985cc095dae3b7f971672';
+// 	    var UCDBoard_Done = '584985d06e1cd3a9c599a190';
+
+// 		var creationSuccess = function(data) {
+// 		  console.log('Card created successfully. Data returned:' + JSON.stringify(data));
+// 		};
+		
+// 		var getSuccess = function(data) {
+// 			console.log(data.labels);
+// 		  	var newCard = {
+// 				name: data.name, 
+// 				desc: data.desc,
+// 				labels: data.labels,
+// 				// Place this card at the top of our list 
+// 				idList: UCDBoard_Triage,
+// 				pos: 'top'
+// 			};
+// 			Trello.post('/cards/', newCard, creationSuccess);
+// 		};
+
+// 		// Trello.cards.get('58487efdfdedd23534be018c',getSuccess);
+
+// 		Trello.boards.get(kanban,getSuccess)
+
+// 	};
+
+// 	var authenticationFailure = function() {
+// 	    console.log("Failed authentication");
+// 	};
+
+// 	$('#update').click(function() {
+// 		Trello.authorize({
+// 			type: 'popup',
+// 			name: 'Getting Started Application',
+// 			scope: {
+// 				read: 'true',
+// 				write: 'true' },
+// 			expiration: 'never',
+// 			success: authenticationSuccess,
+// 			error: authenticationFailure
+// 		});
+// 	});
+// });
--- test.js
+++ test.js
@@ -1,46 +1,499 @@
 {
-    "id": "58487efdfdedd23534be018c",
-    "badges": {
-        "votes": 0,
-        "viewingMemberVoted": false,
-        "subscribed": false,
-        "fogbugz": "",
-        "checkItems": 0,
-        "checkItemsChecked": 0,
-        "comments": 0,
-        "attachments": 0,
-        "description": false,
-        "due": null,
-        "dueComplete": false
-    },
-    "checkItemStates": [],
-    "closed": false,
-    "dueComplete": false,
-    "dateLastActivity": "2016-12-07T21:28:29.083Z",
+    "id": "58487edd7b75ece246c80b59",
+    "name": "Kanban",
     "desc": "",
     "descData": null,
-    "due": null,
-    "email": "dannygosain1+2mwdmcpzhveon73x0lf+2nd130tglubp2ehmfm4+0vuakek88r@boards.trello.com",
-    "idBoard": "58487edd7b75ece246c80b59",
-    "idChecklists": [],
-    "idList": "58487ef55414be087e667eb8",
-    "idMembers": [],
-    "idMembersVoted": [],
-    "idShort": 1,
-    "idAttachmentCover": null,
-    "manualCoverAttachment": false,
-    "labels": [],
-    "idLabels": [],
-    "name": "UCD",
-    "pos": 65535,
-    "shortLink": "Toa4ZQvS",
-    "shortUrl": "https://trello.com/c/Toa4ZQvS",
+    "closed": false,
+    "idOrganization": null,
+    "invited": false,
+    "pinned": false,
+    "starred": false,
+    "url": "https://trello.com/b/GmIpTf75/kanban",
+    "prefs": {
+        "permissionLevel": "private",
+        "voting": "disabled",
+        "comments": "members",
+        "invitations": "members",
+        "selfJoin": false,
+        "cardCovers": true,
+        "cardAging": "regular",
+        "calendarFeedEnabled": false,
+        "background": "blue",
+        "backgroundImage": null,
+        "backgroundImageScaled": null,
+        "backgroundTile": false,
+        "backgroundBrightness": "dark",
+        "backgroundColor": "#0079BF",
+        "canBePublic": true,
+        "canBeOrg": true,
+        "canBePrivate": true,
+        "canInvite": true
+    },
+    "invitations": [],
+    "memberships": [{
+        "id": "58487edd7b75ece246c80b5a",
+        "idMember": "57dadf98777eec8e39595043",
+        "memberType": "admin",
+        "unconfirmed": false,
+        "deactivated": false
+    }],
+    "shortLink": "GmIpTf75",
     "subscribed": false,
-    "url": "https://trello.com/c/Toa4ZQvS/1-ucd",
-    "members": [],
-    "pluginData": [],
+    "labelNames": {
+        "green": "UCD",
+        "yellow": "Jenkins",
+        "orange": "",
+        "red": "",
+        "purple": "",
+        "blue": "",
+        "sky": "",
+        "lime": "",
+        "pink": "",
+        "black": ""
+    },
+    "powerUps": [],
+    "dateLastActivity": "2016-12-08T18:17:22.099Z",
+    "dateLastView": "2016-12-08T18:17:31.067Z",
+    "shortUrl": "https://trello.com/b/GmIpTf75",
+    "idTags": [],
     "checklists": [],
+    "pluginData": [],
+    "labels": [{
+        "id": "58487edd84e677fd36a95451",
+        "idBoard": "58487edd7b75ece246c80b59",
+        "name": "UCD",
+        "color": "green",
+        "uses": 1
+    }, {
+        "id": "58487edd84e677fd36a95452",
+        "idBoard": "58487edd7b75ece246c80b59",
+        "name": "Jenkins",
+        "color": "yellow",
+        "uses": 1
+    }, {
+        "id": "58487edd84e677fd36a95453",
+        "idBoard": "58487edd7b75ece246c80b59",
+        "name": "",
+        "color": "orange",
+        "uses": 0
+    }, {
+        "id": "58487edd84e677fd36a95454",
+        "idBoard": "58487edd7b75ece246c80b59",
+        "name": "",
+        "color": "red",
+        "uses": 0
+    }, {
+        "id": "58487edd84e677fd36a95455",
+        "idBoard": "58487edd7b75ece246c80b59",
+        "name": "",
+        "color": "purple",
+        "uses": 0
+    }, {
+        "id": "58487edd84e677fd36a95456",
+        "idBoard": "58487edd7b75ece246c80b59",
+        "name": "",
+        "color": "blue",
+        "uses": 0
+    }],
+    "members": [{
+        "id": "57dadf98777eec8e39595043",
+        "avatarHash": null,
+        "bio": "",
+        "bioData": null,
+        "confirmed": true,
+        "fullName": "Danny Gosain",
+        "idPremOrgsAdmin": [],
+        "initials": "DG",
+        "memberType": "normal",
+        "products": [],
+        "status": "disconnected",
+        "url": "https://trello.com/dannygosain1",
+        "username": "dannygosain1"
+    }],
+    "cards": [{
+        "id": "58487efdfdedd23534be018c",
+        "checkItemStates": null,
+        "closed": false,
+        "dateLastActivity": "2016-12-08T16:04:40.420Z",
+        "desc": "HELLO SWAPNIL",
+        "descData": {
+            "emoji": {}
+        },
+        "idBoard": "58487edd7b75ece246c80b59",
+        "idList": "58487ef55414be087e667eb8",
+        "idMembersVoted": [],
+        "idShort": 1,
+        "idAttachmentCover": null,
+        "manualCoverAttachment": false,
+        "idLabels": ["58487edd84e677fd36a95451"],
+        "name": "UCD",
+        "pos": 65535,
+        "shortLink": "Toa4ZQvS",
+        "badges": {
+            "votes": 0,
+            "viewingMemberVoted": false,
+            "subscribed": false,
+            "fogbugz": "",
+            "checkItems": 0,
+            "checkItemsChecked": 0,
+            "comments": 0,
+            "attachments": 0,
+            "description": true,
+            "due": null,
+            "dueComplete": false
+        },
+        "dueComplete": false,
+        "due": null,
+        "email": "dannygosain1+2mwdmcpzhveon73x0lf+2nd130tglubp2ehmfm4+0vuakek88r@boards.trello.com",
+        "idChecklists": [],
+        "idMembers": [],
+        "labels": [{
+            "id": "58487edd84e677fd36a95451",
+            "idBoard": "58487edd7b75ece246c80b59",
+            "name": "UCD",
+            "color": "green",
+            "uses": 1
+        }],
+        "shortUrl": "https://trello.com/c/Toa4ZQvS",
+        "subscribed": false,
+        "url": "https://trello.com/c/Toa4ZQvS/1-ucd",
+        "attachments": [],
+        "pluginData": []
+    }, {
+        "id": "5849a049e746caa1d3b70fb1",
+        "checkItemStates": null,
+        "closed": false,
+        "dateLastActivity": "2016-12-08T18:17:22.099Z",
+        "desc": "",
+        "descData": null,
+        "idBoard": "58487edd7b75ece246c80b59",
+        "idList": "584985de7a5f132f3c571359",
+        "idMembersVoted": [],
+        "idShort": 2,
+        "idAttachmentCover": null,
+        "manualCoverAttachment": false,
+        "idLabels": ["58487edd84e677fd36a95452"],
+        "name": "Jenkins",
+        "pos": 131071,
+        "shortLink": "Gk6rx2jL",
+        "badges": {
+            "votes": 0,
+            "viewingMemberVoted": false,
+            "subscribed": false,
+            "fogbugz": "",
+            "checkItems": 0,
+            "checkItemsChecked": 0,
+            "comments": 0,
+            "attachments": 0,
+            "description": false,
+            "due": null,
+            "dueComplete": false
+        },
+        "dueComplete": false,
+        "due": null,
+        "email": "dannygosain1+2mwdmcpzhveon73x0lf+2nd79ho7zc1d5hpgp0x+062s3mby4l@boards.trello.com",
+        "idChecklists": [],
+        "idMembers": [],
+        "labels": [{
+            "id": "58487edd84e677fd36a95452",
+            "idBoard": "58487edd7b75ece246c80b59",
+            "name": "Jenkins",
+            "color": "yellow",
+            "uses": 1
+        }],
+        "shortUrl": "https://trello.com/c/Gk6rx2jL",
+        "subscribed": false,
+        "url": "https://trello.com/c/Gk6rx2jL/2-jenkins",
+        "attachments": [],
+        "pluginData": []
+    }],
+    "lists": [{
+        "id": "58487ef55414be087e667eb8",
+        "name": "Triage",
+        "closed": false,
+        "idBoard": "58487edd7b75ece246c80b59",
+        "pos": 65535,
+        "subscribed": false
+    }, {
+        "id": "584985dca41ddebcf5b67a3b",
+        "name": "Backlog",
+        "closed": false,
+        "idBoard": "58487edd7b75ece246c80b59",
+        "pos": 131071,
+        "subscribed": false
+    }, {
+        "id": "584985de7a5f132f3c571359",
+        "name": "Refined",
+        "closed": false,
+        "idBoard": "58487edd7b75ece246c80b59",
+        "pos": 196607,
+        "subscribed": false
+    }, {
+        "id": "584985e0878a01f42c7dd9c9",
+        "name": "Do it",
+        "closed": false,
+        "idBoard": "58487edd7b75ece246c80b59",
+        "pos": 262143,
+        "subscribed": false
+    }, {
+        "id": "584985e2bcbd2d6587dd07d6",
+        "name": "Done",
+        "closed": false,
+        "idBoard": "58487edd7b75ece246c80b59",
+        "pos": 327679,
+        "subscribed": false
+    }],
     "actions": [{
+        "id": "5849a3b26766940a03c69254",
+        "idMemberCreator": "57dadf98777eec8e39595043",
+        "data": {
+            "listAfter": {
+                "name": "Refined",
+                "id": "584985de7a5f132f3c571359"
+            },
+            "listBefore": {
+                "name": "Backlog",
+                "id": "584985dca41ddebcf5b67a3b"
+            },
+            "board": {
+                "shortLink": "GmIpTf75",
+                "name": "Kanban",
+                "id": "58487edd7b75ece246c80b59"
+            },
+            "card": {
+                "shortLink": "Gk6rx2jL",
+                "idShort": 2,
+                "name": "Jenkins",
+                "id": "5849a049e746caa1d3b70fb1",
+                "idList": "584985de7a5f132f3c571359"
+            },
+            "old": {
+                "idList": "584985dca41ddebcf5b67a3b"
+            }
+        },
+        "type": "updateCard",
+        "date": "2016-12-08T18:17:22.102Z",
+        "memberCreator": {
+            "id": "57dadf98777eec8e39595043",
+            "avatarHash": null,
+            "fullName": "Danny Gosain",
+            "initials": "DG",
+            "username": "dannygosain1"
+        }
+    }, {
+        "id": "5849a3af9a765d4f2f7fa975",
+        "idMemberCreator": "57dadf98777eec8e39595043",
+        "data": {
+            "listAfter": {
+                "name": "Backlog",
+                "id": "584985dca41ddebcf5b67a3b"
+            },
+            "listBefore": {
+                "name": "Triage",
+                "id": "58487ef55414be087e667eb8"
+            },
+            "board": {
+                "shortLink": "GmIpTf75",
+                "name": "Kanban",
+                "id": "58487edd7b75ece246c80b59"
+            },
+            "card": {
+                "shortLink": "Gk6rx2jL",
+                "idShort": 2,
+                "name": "Jenkins",
+                "id": "5849a049e746caa1d3b70fb1",
+                "idList": "584985dca41ddebcf5b67a3b"
+            },
+            "old": {
+                "idList": "58487ef55414be087e667eb8"
+            }
+        },
+        "type": "updateCard",
+        "date": "2016-12-08T18:17:19.889Z",
+        "memberCreator": {
+            "id": "57dadf98777eec8e39595043",
+            "avatarHash": null,
+            "fullName": "Danny Gosain",
+            "initials": "DG",
+            "username": "dannygosain1"
+        }
+    }, {
+        "id": "5849a049e746caa1d3b70fb2",
+        "idMemberCreator": "57dadf98777eec8e39595043",
+        "data": {
+            "board": {
+                "shortLink": "GmIpTf75",
+                "name": "Kanban",
+                "id": "58487edd7b75ece246c80b59"
+            },
+            "list": {
+                "name": "Triage",
+                "id": "58487ef55414be087e667eb8"
+            },
+            "card": {
+                "shortLink": "Gk6rx2jL",
+                "idShort": 2,
+                "name": "Jenkins",
+                "id": "5849a049e746caa1d3b70fb1"
+            }
+        },
+        "type": "createCard",
+        "date": "2016-12-08T18:02:49.544Z",
+        "memberCreator": {
+            "id": "57dadf98777eec8e39595043",
+            "avatarHash": null,
+            "fullName": "Danny Gosain",
+            "initials": "DG",
+            "username": "dannygosain1"
+        }
+    }, {
+        "id": "584985e2bcbd2d6587dd07d7",
+        "idMemberCreator": "57dadf98777eec8e39595043",
+        "data": {
+            "board": {
+                "shortLink": "GmIpTf75",
+                "name": "Kanban",
+                "id": "58487edd7b75ece246c80b59"
+            },
+            "list": {
+                "name": "Done",
+                "id": "584985e2bcbd2d6587dd07d6"
+            }
+        },
+        "type": "createList",
+        "date": "2016-12-08T16:10:10.168Z",
+        "memberCreator": {
+            "id": "57dadf98777eec8e39595043",
+            "avatarHash": null,
+            "fullName": "Danny Gosain",
+            "initials": "DG",
+            "username": "dannygosain1"
+        }
+    }, {
+        "id": "584985e0878a01f42c7dd9ca",
+        "idMemberCreator": "57dadf98777eec8e39595043",
+        "data": {
+            "board": {
+                "shortLink": "GmIpTf75",
+                "name": "Kanban",
+                "id": "58487edd7b75ece246c80b59"
+            },
+            "list": {
+                "name": "Do it",
+                "id": "584985e0878a01f42c7dd9c9"
+            }
+        },
+        "type": "createList",
+        "date": "2016-12-08T16:10:08.939Z",
+        "memberCreator": {
+            "id": "57dadf98777eec8e39595043",
+            "avatarHash": null,
+            "fullName": "Danny Gosain",
+            "initials": "DG",
+            "username": "dannygosain1"
+        }
+    }, {
+        "id": "584985de7a5f132f3c57135a",
+        "idMemberCreator": "57dadf98777eec8e39595043",
+        "data": {
+            "board": {
+                "shortLink": "GmIpTf75",
+                "name": "Kanban",
+                "id": "58487edd7b75ece246c80b59"
+            },
+            "list": {
+                "name": "Refined",
+                "id": "584985de7a5f132f3c571359"
+            }
+        },
+        "type": "createList",
+        "date": "2016-12-08T16:10:06.494Z",
+        "memberCreator": {
+            "id": "57dadf98777eec8e39595043",
+            "avatarHash": null,
+            "fullName": "Danny Gosain",
+            "initials": "DG",
+            "username": "dannygosain1"
+        }
+    }, {
+        "id": "584985dca41ddebcf5b67a3c",
+        "idMemberCreator": "57dadf98777eec8e39595043",
+        "data": {
+            "board": {
+                "shortLink": "GmIpTf75",
+                "name": "Kanban",
+                "id": "58487edd7b75ece246c80b59"
+            },
+            "list": {
+                "name": "Backlog",
+                "id": "584985dca41ddebcf5b67a3b"
+            }
+        },
+        "type": "createList",
+        "date": "2016-12-08T16:10:04.004Z",
+        "memberCreator": {
+            "id": "57dadf98777eec8e39595043",
+            "avatarHash": null,
+            "fullName": "Danny Gosain",
+            "initials": "DG",
+            "username": "dannygosain1"
+        }
+    }, {
+        "id": "584985a16522ebcc867efd26",
+        "idMemberCreator": "57dadf98777eec8e39595043",
+        "data": {
+            "board": {
+                "shortLink": "GmIpTf75",
+                "id": "58487edd7b75ece246c80b59",
+                "name": "Kanban"
+            },
+            "old": {
+                "name": "Board 1"
+            }
+        },
+        "type": "updateBoard",
+        "date": "2016-12-08T16:09:05.140Z",
+        "memberCreator": {
+            "id": "57dadf98777eec8e39595043",
+            "avatarHash": null,
+            "fullName": "Danny Gosain",
+            "initials": "DG",
+            "username": "dannygosain1"
+        }
+    }, {
+        "id": "5848897fa2fbae4ae7565f9c",
+        "idMemberCreator": "57dadf98777eec8e39595043",
+        "data": {
+            "list": {
+                "name": "Triage",
+                "id": "58487ef55414be087e667eb8"
+            },
+            "board": {
+                "shortLink": "GmIpTf75",
+                "name": "Board 1",
+                "id": "58487edd7b75ece246c80b59"
+            },
+            "card": {
+                "shortLink": "Toa4ZQvS",
+                "idShort": 1,
+                "name": "UCD",
+                "id": "58487efdfdedd23534be018c",
+                "desc": "HELLO SWAPNIL"
+            },
+            "old": {
+                "desc": ""
+            }
+        },
+        "type": "updateCard",
+        "date": "2016-12-07T22:13:19.198Z",
+        "memberCreator": {
+            "id": "57dadf98777eec8e39595043",
+            "avatarHash": null,
+            "fullName": "Danny Gosain",
+            "initials": "DG",
+            "username": "dannygosain1"
+        }
+    }, {
         "id": "58487efdfdedd23534be018d",
         "idMemberCreator": "57dadf98777eec8e39595043",
         "data": {
@@ -69,11 +522,73 @@
             "initials": "DG",
             "username": "dannygosain1"
         }
+    }, {
+        "id": "58487efab8afdc4ad1ed99f3",
+        "idMemberCreator": "57dadf98777eec8e39595043",
+        "data": {
+            "board": {
+                "shortLink": "GmIpTf75",
+                "name": "Board 1",
+                "id": "58487edd7b75ece246c80b59"
+            },
+            "list": {
+                "id": "58487ef55414be087e667eb8",
+                "name": "Triage"
+            },
+            "old": {
+                "name": "UCD"
+            }
+        },
+        "type": "updateList",
+        "date": "2016-12-07T21:28:26.467Z",
+        "memberCreator": {
+            "id": "57dadf98777eec8e39595043",
+            "avatarHash": null,
+            "fullName": "Danny Gosain",
+            "initials": "DG",
+            "username": "dannygosain1"
+        }
+    }, {
+        "id": "58487ef55414be087e667eb9",
+        "idMemberCreator": "57dadf98777eec8e39595043",
+        "data": {
+            "board": {
+                "shortLink": "GmIpTf75",
+                "name": "Board 1",
+                "id": "58487edd7b75ece246c80b59"
+            },
+            "list": {
+                "name": "UCD",
+                "id": "58487ef55414be087e667eb8"
+            }
+        },
+        "type": "createList",
+        "date": "2016-12-07T21:28:21.561Z",
+        "memberCreator": {
+            "id": "57dadf98777eec8e39595043",
+            "avatarHash": null,
+            "fullName": "Danny Gosain",
+            "initials": "DG",
+            "username": "dannygosain1"
+        }
+    }, {
+        "id": "58487edd7b75ece246c80b5b",
+        "idMemberCreator": "57dadf98777eec8e39595043",
+        "data": {
+            "board": {
+                "shortLink": "GmIpTf75",
+                "name": "Board 1",
+                "id": "58487edd7b75ece246c80b59"
+            }
+        },
+        "type": "createBoard",
+        "date": "2016-12-07T21:27:57.470Z",
+        "memberCreator": {
+            "id": "57dadf98777eec8e39595043",
+            "avatarHash": null,
+            "fullName": "Danny Gosain",
+            "initials": "DG",
+            "username": "dannygosain1"
+        }
     }]
-} // This is just a sample script. Paste your real code (javascript or HTML) here.
-
-if ('this_is' == /an_example/) {
-    of_beautifier();
-} else {
-    var a = b ? (c % d) : e[f];
 }
