ae2850d1b218c83c69dd9fd6ff64653ce91a3ee8
Danny Gosain <dgosain@uwaterloo.ca>
2016-12-20 14:46:07 -0500

    adding functionality to auto update

--- autoRefresh.js
+++ autoRefresh.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
 	var limit = 0;
-	while(limit < 20){
-		setTimeout(function() {
-			$('#test').trigger('click');
-		},5000);
-	limit++;
-	}
 
 	$('#test').click(function() {
-		console.log(limit);
-		console.log('#test Clicked');
+		console.log("90CM!!!!");
+		setTimeout(function() {
+			$('#test').trigger('click');
+		},1000);
 	});
 
 });
